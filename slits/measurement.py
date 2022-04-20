285f86f8590454339596b79c4ed027b907e49a81
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2022-04-20 21:58:36 +0300

    added measurement jitter fix options

--- slits/measurement.py
+++ slits/measurement.py
@@ -1,11 +1,13 @@
 import numpy as np
+from operator import itemgetter
 from dataclasses import dataclass
 
-from .utils import align
+from .utils import align, filter_ascending, unzip
 
 # M, N = 0.011054427827757713, -0.000484659074423251  # Calibration linear fit parameters
 M = 0.10369058652068087
 N = 0.0005657451071065812
+FIX = np.pi
 
 
 def v2x(v):
@@ -44,3 +46,10 @@ class Measurement:
 
     def to_csv(self):
         return "\n".join(map(lambda t: f"{t[0]}, {t[1]}", zip(self.x, self.intensity)))
+
+    def sort_jitter(self):
+        return Measurement(self.metadata, self.index, self.time, self.channel0, np.array(list(sorted(self.channel1))))
+
+    def skip_jitter(self):
+        channel0, channel1 = list(unzip(filter_ascending(list(zip(self.channel0, self.channel1)), key=itemgetter(1))))
+        return Measurement(self.metadata, self.index, self.time, np.array(list(channel0)), np.array(list(channel1)))
--- slits/utils.py
+++ slits/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import reduce
 
 from .setup import Z
 
@@ -9,3 +10,15 @@ def angle2x(angle):
 
 def align(x_data, y_data):
     return x_data - x_data[np.argmax(y_data)]
+
+
+def identity(x):
+    return x
+
+
+def filter_ascending(items, key=identity):
+    return reduce(lambda acc, cur: (acc + [cur]) if key(cur) >= key(acc[-1]) else acc, items[1:], [items[0]])
+
+
+def unzip(tuples):
+    return zip(*tuples)
